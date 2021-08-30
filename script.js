8f38982b3b45d7b49fb3fd12e9b8ad4cc2ae796b
森祐一朗 <apotikisin4632@gmail.com>
2021-08-30 21:42:00 +0900

    split screen

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log('Hello, World!');
+var map;
 
 navigator.geolocation.getCurrentPosition(
     (position) => {
         const { latitude } = position.coords;
         const { longitude } = position.coords;
-        console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
+        
         const coords = [latitude, longitude];
 
-        const map = L.map('mapid').setView(coords, 13);
+        map = L.map('mapid');
+        map.setView(coords, 13);
 
         const data = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -16,12 +16,26 @@ navigator.geolocation.getCurrentPosition(
 
         data.addTo(map);
 
-        const marker = L.marker(coords).addTo(map)
+        // Call OnMarker function
+        map.on('click', OnMarker);
 
-        marker.bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
-        console.log('Finish!');
+        console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
     },
     () => {
         alert('Could not get your position');
     }
 );
+
+//Set Marker on the map
+const OnMarker = (e) => {
+    console.log('Set, marker!');
+
+    const marker = L.marker(e.latlng).on('click', DelMarker).addTo(map)
+
+    marker.bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+};
+
+//Delete marker when marker is clicked
+const DelMarker = (e) => {
+    map.removeLayer(e.target);
+};
